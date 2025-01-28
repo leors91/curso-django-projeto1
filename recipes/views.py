8d4ab2c7b3cc33d7849eb8131b0356b4e8b76583
Leonardo Rodrigues <leorsantos657@gmail.com>
2025-01-28 20:53:42 -0300

    aula_23

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,22 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
+# def home(request):
+#     return HttpResponse('''<!DOCTYPE>
+#     <html>
+#     <head><title>Olá mundo</title></head>
+
+#     <body>
+#         <h1>Olá mundo</h1>
+#     <body>
+
+
+#     </html>
+#     ''')
+
 def home(request):
-    return HttpResponse('HOME 2')
+    return render(request, 'recipes/home.html')
 
 def contato(request):
     return HttpResponse('contato 40')
